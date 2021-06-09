abc88f36f622c9a66111ce6f361b706e6136bd1b
urigross <urigross@gmail.com>
2021-06-09 21:31:28 +0300

    Text on Canvas.

--- js/canvas-controller.js
+++ js/canvas-controller.js
@@ -9,3 +9,17 @@ function drawImage(imageId) {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
     }
 }
+
+function onTextLine(txt) {
+    drawText(txt, 180, 50);
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'red'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '20px Arial'
+    gCtx.textAlign = 'left'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
